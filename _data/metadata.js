07324b3479d2211bc57ea9b403fc508e96c101be
Tim Huddle <blazhen88@gmail.com>
2023-01-27 20:42:47 -0500

    barebones eleventy site + ghpages deployment

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	"title": "Huddle's Dev Thoughts",
+	"url": "https://www.timhuddle.dev/",
+	"language": "en",
+	"description": "I write all the javascript. Sometimes I write about writing all the javascript",
+	"author": {
+		"name": "Tim Huddle",
+		"email": "tim@timhuddle.dev",
+		"url": "https://www.timhuddle.dev/about/"
+	}
+}
---
+++ content/blog/hello-world.md
@@ -0,0 +1,10 @@
+---
+title: Hello World
+description: My first proper blog post on my first proper personal website.
+date: 2023-01-27
+draft: true
+tags:
+  - lit
+  - codemash
+---
+This would be where I blogged about my experiences talking about Lit at CodeMash 2023.
